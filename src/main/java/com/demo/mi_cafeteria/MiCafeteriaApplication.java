7050fc02018c6e3b56e7cdc4b2c163ab5e8c91dc
AndyCarmenR <andrescarmenromero@gmail.com>
2025-04-05 14:24:36 -0600

    creado proyecto java springboot

---
+++ src/main/java/com/demo/mi_cafeteria/MiCafeteriaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mi_cafeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiCafeteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiCafeteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/mi_cafeteria/MiCafeteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.mi_cafeteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiCafeteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
