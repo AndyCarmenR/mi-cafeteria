b200b86d40396bdd180e747ab6ef89f78a4788b5
AndyCarmenR <andrescarmenromero@gmail.com>
2025-07-09 19:56:48 -0600

    agregada implementacion de swagger para documentacion y testeo de endpoints

--- src/main/java/com/demo/mi_cafeteria/security/SecurityConfig.java
+++ src/main/java/com/demo/mi_cafeteria/security/SecurityConfig.java
@@ -20,7 +20,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login").permitAll()
+                        .requestMatchers("/auth/login",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
